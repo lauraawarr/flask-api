d6b0c4fe170098a6bfcd0ca33716c05b7a3d78b1
Laura Warr <lauraawarr@gmail.com>
2019-12-02 21:33:18 -0800

    Remove config

--- config.py
+++
@@ -1,4 +0,0 @@
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:keyhole123@flask-api.cfchkkfvtumx.us-east-2.rds.amazonaws.com:3306/flaskapi'
-
-SQLALCHEMY_POOL_RECYCLE = 3600
-SQLALCHEMY_TRACK_MODIFICATIONS = False
